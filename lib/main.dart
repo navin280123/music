e6826f1305ccce8ced6171dcec70ae648ff553a6
Navin Kumar Verma <kumarnavinverma7@gmail.com>
2024-10-31 17:06:26 +0530

    Update animation controller to repeat and upgrade dependencies in pubspec.lock

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,7 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
     _animationController = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 5000),
-    );
+    )..repeat();
 
     startAppSetup();
   }
